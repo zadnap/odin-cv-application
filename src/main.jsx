2d49eabf09c0587caefc801f393943146b02d36d
zadnap <zadnap@users.noreply.github.com>
2025-04-04 15:49:53 +0700

    feat: initialize repository
    The project currently contains favicon, basic scss for styling, and
    configuration files for ESLint and Prettier.

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.scss';
+
+createRoot(document.getElementById('root')).render(<StrictMode></StrictMode>);
