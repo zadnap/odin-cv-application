d03ddd4e716280972000766c415d128972717481
zadnap <zadnap@users.noreply.github.com>
2025-04-10 10:23:49 +0700

    refactor(UI): remove subSection
    Now, the subSection component is represented by section nested inside
    section. This makes the code less repetetive and cleaner.

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,10 +1,26 @@
 import styles from '@/styles/section.module.scss';
+import Input from './Input';
 
-function Section({ title, children }) {
+function Section({ title, description, isEditing, children }) {
   return (
     <section className={styles.section}>
-      <h1>{title}</h1>
-      <div className={styles.content}>{children}</div>
+      {title && (
+        <h1>
+          {isEditing ? (
+            <Input
+              placeholder={title}
+              ariaLabel={`Edit ${title}`}
+              value={title}
+            />
+          ) : (
+            <span className={styles.text}>{title}</span>
+          )}
+        </h1>
+      )}
+      <div className={styles.content}>
+        {description && <div className={styles.description}>{description}</div>}
+        {children && <div className={styles.children}>{children}</div>}
+      </div>
     </section>
   );
 }
