d0a1ae6fb11e814a2c0e8f40d2aef0fd5ea156fd
zadnap <zadnap@users.noreply.github.com>
2025-04-11 20:03:24 +0700

    fix(Input): add auto expansion when inputing
    When the input content is increased in terms of text length, its width
    is increased according to the content's length.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,15 +1,34 @@
+import { useRef, useEffect, useState } from 'react';
 import styles from '@/styles/input.module.scss';
 
-function Input({ placeholder, ariaLabel, value, onChange, type = 'text' }) {
+function Input({ placeholder, ariaLabel, value, onChange }) {
+  const inputRef = useRef(null);
+  const spanRef = useRef(null);
+  const [inputWidth, setInputWidth] = useState('auto');
+
+  useEffect(() => {
+    if (spanRef.current) {
+      const newWidth = spanRef.current.offsetWidth;
+      setInputWidth(`${newWidth}px`);
+    }
+  }, [value]);
+
   return (
-    <input
-      className={styles.input}
-      placeholder={placeholder}
-      aria-label={ariaLabel}
-      value={value}
-      type={type}
-      onChange={onChange}
-    />
+    <div className={styles.inputWrapper}>
+      <input
+        ref={inputRef}
+        className={styles.input}
+        placeholder={placeholder}
+        aria-label={ariaLabel}
+        value={value}
+        onChange={onChange}
+        style={{ width: inputWidth }}
+      />
+      <span ref={spanRef} className={styles.hiddenSpan}>
+        {value || placeholder}
+      </span>
+    </div>
   );
 }
+
 export default Input;
