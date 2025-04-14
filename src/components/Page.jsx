32d7d509d3356e6544129f03d0e84d0413657f2e
zadnap <zadnap@users.noreply.github.com>
2025-04-14 20:20:52 +0700

    refactor(Page): refine the page layout and styles

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -3,10 +3,8 @@ import styles from '@/styles/page.module.scss';
 function Page({ isEditing }) {
   return (
     <section className={styles.page}>
-      <aside className={styles.sidebar}>
-        <div className={styles.info}></div>
-      </aside>
-      <section isEditing={isEditing} className={styles.main}></section>
+      <aside className={styles.sidebar}></aside>
+      <section className={styles.main}></section>
     </section>
   );
 }
