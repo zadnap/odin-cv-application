17cacfe929b0ffcda9d58ca626595578a4e23903
zadnap <zadnap@users.noreply.github.com>
2025-04-14 21:00:06 +0700

    feat(Avatar): add image changing button

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,10 +1,24 @@
 import styles from '@/styles/avatar.module.scss';
 import avatarSrc from '@/assets/default-avatar.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCameraRetro } from '@fortawesome/free-solid-svg-icons';
 
-function Avatar() {
+function Avatar({ src, onChange, isEditing }) {
   return (
     <div className={styles.avatar}>
-      <img src={avatarSrc} alt="User's Avatar" />
+      <img src={src || avatarSrc} alt="User's Avatar" />
+      {isEditing && (
+        <label htmlFor="avatarInput" className={styles.avatarInputLabel}>
+          <FontAwesomeIcon icon={faCameraRetro} />
+        </label>
+      )}
+      <input
+        className={styles.avatarInput}
+        type="file"
+        id="avatarInput"
+        accept="image/*"
+        onChange={onChange}
+      />
     </div>
   );
 }
