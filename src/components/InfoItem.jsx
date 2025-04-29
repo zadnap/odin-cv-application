ae115ee1cef67c306158a5d8a8a920767b6cec2d
zadnap <zadnap@users.noreply.github.com>
2025-04-29 07:55:28 +0700

    feat(InfoItem): add InfoItem component
    This component contains information about jobs or studies that have been
    carried out so far that the CV owner wants to add to their CV.

---
+++ src/components/InfoItem.jsx
@@ -0,0 +1,16 @@
+import styles from '@/styles/InfoItem.module.scss';
+
+function InfoItem({ title, subtitle, from, to, desc }) {
+  return (
+    <div className={styles.infoItem}>
+      <h4 className={styles.subtitle}>{subtitle}</h4>
+      <h3 className={styles.title}>{title}</h3>
+      <p className={styles.dateRange}>
+        {from} - {to}
+      </p>
+      <p className={styles.desc}>{desc}</p>
+    </div>
+  );
+}
+
+export default InfoItem;
