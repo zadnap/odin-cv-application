3d55dcf1fa18315142f4c3089d547420fe367cb1
zadnap <zadnap@users.noreply.github.com>
2025-04-29 11:30:49 +0700

    feat(cvData): add cvData state and render in CV

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,57 @@
+import { useState } from 'react';
 import styles from './App.module.scss';
 import EditForm from './components/EditForm';
 import Resume from './components/Resume';
 
 function App() {
+  const [cvData, setCvData] = useState({
+    personal: {
+      fullname: 'Jennifer Watson',
+      title: 'User Interface Designer',
+      aboutMe:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sit quisquam optio libero, ipsa labore dolorem odit expedita numquam eum quos mollitia rem et natus quo animi officiis. Magni, similique?',
+      address: '12th Avenue Street Australia 40000',
+      email: 'johnwatson@gmail.com',
+      phone: '02800200',
+    },
+    education: [
+      {
+        school: 'University of Lorem',
+        degree: 'Certificate of Webtraining',
+        start: 2015,
+        end: 2017,
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sit',
+      },
+      {
+        school: 'University of Lorem',
+        degree: 'Certificate of Webtraining',
+        start: 2015,
+        end: 2017,
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sit',
+      },
+    ],
+    experience: [
+      {
+        company: 'Soft Design Studios',
+        title: 'Graphic Designer',
+        start: 2015,
+        end: 2017,
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sit',
+      },
+      {
+        company: 'Soft Design Studios',
+        title: 'Graphic Designer',
+        start: 2015,
+        end: 2017,
+        desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sit',
+      },
+    ],
+  });
+
   return (
     <div className={styles.app}>
-      <EditForm />
-      <Resume />
+      <EditForm cvData={cvData} />
+      <Resume cvData={cvData} />
     </div>
   );
 }
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -4,76 +4,41 @@ import Section from './Section';
 import Contact from './Contact';
 import InfoItem from './InfoItem';
 
-function Resume() {
+function Resume({ cvData }) {
   return (
     <div className={styles.resume}>
-      <Headline
-        name="Jennifer Watson"
-        title="User Interface Designer"
-        webpage="www.jwatson.com"
-      />
+      <Headline name={cvData.personal.fullname} title={cvData.personal.title} />
       <div className={styles.body}>
         <div className={styles.left}>
-          <Section title="About Me">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sit
-            quisquam optio libero, ipsa labore dolorem odit expedita numquam eum
-            quos mollitia rem et natus quo animi officiis. Magni, similique?
-          </Section>
+          <Section title="About Me">{cvData.personal.aboutMe}</Section>
           <Section title="Contact">
-            <Contact
-              name="Address"
-              content="12th Avenue Street Australia 40000"
-            />
-            <Contact name="Email" content="johnwatson@gmail.com" />
-            <Contact name="Phone" content="02800200" />
+            <Contact name="Address" content={cvData.personal.address} />
+            <Contact name="Email" content={cvData.personal.email} />
+            <Contact name="Phone" content={cvData.personal.phone} />
           </Section>
         </div>
         <div className={styles.right}>
-          <Section title="Experiences">
-            <InfoItem
-              title="Soft Design Studios"
-              subtitle="Graphic Designer"
-              from={2015}
-              to={2017}
-              desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sit"
-            />
-            <InfoItem
-              title="Soft Design Studios"
-              subtitle="Graphic Designer"
-              from={2015}
-              to={2017}
-              desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sit"
-            />
-            <InfoItem
-              title="Soft Design Studios"
-              subtitle="Graphic Designer"
-              from={2015}
-              to={2017}
-              desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sit"
-            />
-          </Section>
           <Section title="Education">
-            <InfoItem
-              title="Certificate of Webtraining"
-              subtitle="University of Lorem"
-              from={2015}
-              to={2017}
-              desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sit"
-            />
-            <InfoItem
-              title="Certificate of Webtraining"
-              subtitle="University of Lorem"
-              from={2015}
-              to={2017}
-              desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sit"
-            />
-            <InfoItem
-              title="Certificate of Webtraining"
-              subtitle="University of Lorem"
-              from={2015}
-              to={2017}
-              desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sit"
-            />
+            {cvData.education.map((edu) => (
+              <InfoItem
+                title={edu.degree}
+                subtitle={edu.school}
+                from={edu.start}
+                to={edu.end}
+                desc={edu.desc}
+              />
+            ))}
+          </Section>
+          <Section title="Experiences">
+            {cvData.experience.map((exp) => (
+              <InfoItem
+                title={exp.title}
+                subtitle={exp.company}
+                from={exp.start}
+                to={exp.end}
+                desc={exp.desc}
+              />
+            ))}
           </Section>
         </div>
       </div>
